a010ae1ae3c6ea334d7843eab6491fb946aaf290
Matthew B. Reisch <m.reisch@amalgamatedanalytics.com>
2016-02-18 15:43:35 -0500

    First commit

---
+++ bloggy_project/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+# Create your models here.
+class Post(models.Model):
+    created_at = models.DateField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    def __unicode__(self):
+        return self.title
