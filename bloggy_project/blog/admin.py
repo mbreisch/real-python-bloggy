7151e2c6d6507174d0e978cfbc7736f9ba3cfb0a
Matthew B. Reisch <m.reisch@amalgamatedanalytics.com>
2016-02-19 17:35:27 -0500

    Added template functionality

--- bloggy_project/blog/admin.py
+++ bloggy_project/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','created_at')
 
 # Register your models here.
+admin.site.register(Post,PostAdmin)
+
+
